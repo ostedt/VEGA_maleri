e9a227a64a2b6250dea6e5e4623de11e99bd9822
SKYNET\osted_000 <ostedt01@gmail.com>
2016-12-15 10:38:42 +0100

    start hirarchy

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+
+			<meta charset="utf-8">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+			<?php wp_head(); ?>
+
+		</head>
+
+		<body>
