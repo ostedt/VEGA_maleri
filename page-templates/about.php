d4069a1c7f1db1689be9f23834747ddc9bfac7f6
SKYNET\osted_000 <ostedt01@gmail.com>
2017-03-04 20:58:11 +0100

    About core

--- page-templates/about.php
+++ page-templates/about.php
@@ -7,5 +7,53 @@
 <?php get_header(); ?>
 
 
+<?php if( get_field('part1_about_heading') ) :?>
+    <h2><?php the_field('part1_about_heading'); ?></h2>
+    <div class="h2-underline-container">
+    	<div class="h2-underline"></div>
+    </div>
+    
+  <?php if( get_field('part1_about_breadtext') ) : ?>
+    <?php the_field('part1_about_breadtext'); ?>
+  <?php endif; ?>
+
+  <?php if( get_field('part1_about_img') ): ?>
+  	<div id="" class="" style="background-image: url('<?php echo the_field("part1_about_img"); ?>')"></div>
+  <?php endif; ?>
+
+<?php endif; ?>
+
+<?php if( get_field('part2_about_heading') ) :?>
+
+  <?php if( get_field('part2_about_img') ): ?>
+  	<div id="" class="" style="background-image: url('<?php echo the_field("part2_about_img"); ?>')"></div>
+  <?php endif; ?>
+
+    <h2><?php the_field('part2_about_heading'); ?></h2>
+    <div class="h2-underline-container">
+    	<div class="h2-underline"></div>
+    </div>
+    
+  <?php if( get_field('part2_about_breadtext') ) : ?>
+    <?php the_field('part2_about_breadtext'); ?>
+  <?php endif; ?>
+
+<?php endif; ?>
+
+<?php if( get_field('part3_about_heading') ) :?>
+    <h2><?php the_field('part3_about_heading'); ?></h2>
+    <div class="h2-underline-container">
+    	<div class="h2-underline"></div>
+    </div>
+    
+  <?php if( get_field('part3_about_breadtext') ) : ?>
+    <?php the_field('part3_about_breadtext'); ?>
+  <?php endif; ?>
+
+  <?php if( get_field('part3_about_img') ): ?>
+  	<div id="" class="" style="background-image: url('<?php echo the_field("part3_about_img"); ?>')"></div>
+  <?php endif; ?>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
