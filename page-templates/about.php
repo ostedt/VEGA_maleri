5dcb427761d6b4fdf58d1af2a68e8a88ef34b445
SKYNET\osted_000 <ostedt01@gmail.com>
2017-03-04 21:28:08 +0100

    Adding core structure to about page

--- page-templates/about.php
+++ page-templates/about.php
@@ -6,54 +6,65 @@
 
 <?php get_header(); ?>
 
-
 <?php if( get_field('part1_about_heading') ) :?>
-    <h2><?php the_field('part1_about_heading'); ?></h2>
-    <div class="h2-underline-container">
-    	<div class="h2-underline"></div>
-    </div>
-    
-  <?php if( get_field('part1_about_breadtext') ) : ?>
-    <?php the_field('part1_about_breadtext'); ?>
-  <?php endif; ?>
+	<div id="part1" class="about-page-section">
+		<div>
+		    <h1><?php the_field('part1_about_heading'); ?></h1>
+		    <div class="h2-underline-container">
+		    	<div class="h2-underline"></div>
+		    </div>
+		    
+		  <?php if( get_field('part1_about_breadtext') ) : ?>
+		    <?php the_field('part1_about_breadtext'); ?>
+		  <?php endif; ?>
+		</div>
 
-  <?php if( get_field('part1_about_img') ): ?>
-  	<div id="" class="" style="background-image: url('<?php echo the_field("part1_about_img"); ?>')"></div>
-  <?php endif; ?>
+	  <?php if( get_field('part1_about_img') ): ?>
+	  	<div id="" class="" style="background-image: url('<?php echo the_field("part1_about_img"); ?>')"></div>
+	  <?php endif; ?>
 
+	</div>
 <?php endif; ?>
 
 <?php if( get_field('part2_about_heading') ) :?>
+	<div id="part2" class="about-page-section">
 
-  <?php if( get_field('part2_about_img') ): ?>
-  	<div id="" class="" style="background-image: url('<?php echo the_field("part2_about_img"); ?>')"></div>
-  <?php endif; ?>
+		  <?php if( get_field('part2_about_img') ): ?>
+		  	<div id="" class="" style="background-image: url('<?php echo the_field("part2_about_img"); ?>')"></div>
+		  <?php endif; ?>
 
-    <h2><?php the_field('part2_about_heading'); ?></h2>
-    <div class="h2-underline-container">
-    	<div class="h2-underline"></div>
-    </div>
-    
-  <?php if( get_field('part2_about_breadtext') ) : ?>
-    <?php the_field('part2_about_breadtext'); ?>
-  <?php endif; ?>
-
+		<div>
+		    <h2><?php the_field('part2_about_heading'); ?></h2>
+		    <div class="h2-underline-container">
+		    	<div class="h2-underline"></div>
+		    </div>
+		    
+		  <?php if( get_field('part2_about_breadtext') ) : ?>
+		    <?php the_field('part2_about_breadtext'); ?>
+		  <?php endif; ?>
+		</div>
+	</div>
 <?php endif; ?>
 
 <?php if( get_field('part3_about_heading') ) :?>
-    <h2><?php the_field('part3_about_heading'); ?></h2>
-    <div class="h2-underline-container">
-    	<div class="h2-underline"></div>
-    </div>
-    
-  <?php if( get_field('part3_about_breadtext') ) : ?>
-    <?php the_field('part3_about_breadtext'); ?>
-  <?php endif; ?>
+	<div id="part3" class="about-page-section">
 
-  <?php if( get_field('part3_about_img') ): ?>
-  	<div id="" class="" style="background-image: url('<?php echo the_field("part3_about_img"); ?>')"></div>
-  <?php endif; ?>
+		<div>
+		    <h2><?php the_field('part3_about_heading'); ?></h2>
+		    <div class="h2-underline-container">
+		    	<div class="h2-underline"></div>
+		    </div>
+		    
+			  <?php if( get_field('part3_about_breadtext') ) : ?>
+			    <?php the_field('part3_about_breadtext'); ?>
+			  <?php endif; ?>
+		  </div>
 
+	  <?php if( get_field('part3_about_img') ): ?>
+	  	<div id="" class="" style="background-image: url('<?php echo the_field("part3_about_img"); ?>')"></div>
+	  <?php endif; ?>
+
+	</div>
 <?php endif; ?>
 
 <?php get_footer(); ?>
