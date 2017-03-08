b50aaa06de08c02b4b40af482b0b35856fde2e4f
SKYNET\osted_000 <ostedt01@gmail.com>
2017-03-08 19:36:25 +0100

    Edit some core of footer

--- footer.php
+++ footer.php
@@ -1,24 +1,12 @@
+   	<footer>
+	    <?php
+	        if ( is_active_sidebar( 'footer-menu' ) ) {
+	            dynamic_sidebar( 'footer-menu' );
+	        }
+	    ?>
+    </footer>
+
 <?php wp_footer(); ?>
-<div class="footer section">
-
-	<div class="container">
-		
-		<div class="row">
-			
-			<div class="footer-container">
-				
-				<div class="f-1"></div>
-				<div class="f-2"></div>
-				<div class="f-3"></div>
-				<div class="f-4"></div>
-
-			</div>
-
-		</div>
-
-	</div>
-
-</div>
 
 </body>
 </html>
