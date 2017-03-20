42a5f22e7ad4146e1e06de67e51deabb99c229cb
SKYNET\osted_000 <ostedt01@gmail.com>
2017-03-20 22:24:10 +0100

    add grid container to 'archivepage '

--- page-templates/case.php
+++ page-templates/case.php
@@ -10,11 +10,17 @@
     
     <?php while ( have_posts() ) : the_post(); ?>
 
+    	<div class="container">
+    	<div class="row">
+
         <h1 class=""><?php the_title(); ?></h1>
 			<div class="h2-underline-container">
 		    <div class="h2-underline"></div>
         
         <?php VEGA_get_latest_cases(); ?>
+
+        </div>
+        </div>
         
     <?php endwhile; ?>
 
