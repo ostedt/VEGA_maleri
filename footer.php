9d24c5bfbb73435b12bce7d812caf0b20f68c508
SKYNET\osted_000 <ostedt01@gmail.com>
2017-03-28 22:15:31 +0200

    grid footer

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <div class="container">
-<div class="row">   
+<div class="row"> 
+	<div class="icon-container col-xs-12">
+		<div class="col-xs-12 col-md-4"></div>
+		<div class="col-xs-12 col-md-4"></div>
+		<div class="col-xs-12 col-md-4"></div>
+	</div>  
    	<footer>
 		    <?php
 		        if ( is_active_sidebar( 'footer-menu' ) ) {
